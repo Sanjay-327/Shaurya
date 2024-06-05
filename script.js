1a5635fff9cc5dc5204f472a045868d1b09c6006
Sanjay-327 <146809133+Sanjay-327@users.noreply.github.com>
2024-06-05 13:58:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+function myMenuFunction() {
+    var i = document.getElementById("navMenu");
+
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
+
+   var a = document.getElementById("loginBtn");
+   var b = document.getElementById("registerBtn");
+   var x = document.getElementById("login");
+   var y = document.getElementById("register");
+
+   function login() {
+       x.style.left = "4px";
+       y.style.right = "-520px";
+       a.className += " white-btn";
+       b.className = "btn";
+       x.style.opacity = 1;
+       y.style.opacity = 0;
+   }
+
+   function register() {
+       x.style.left = "-510px";
+       y.style.right = "5px";
+       a.className = "btn";
+       b.className += " white-btn";
+       x.style.opacity = 0;
+       y.style.opacity = 1;
+   }
+
+
+let counter = 0;
+
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+        
+        if (this.checkValidity()) {
+            event.preventDefault(); 
+            counter++;
+            document.getElementById('counter').textContent = counter;
+                    
+        } else {
+            
+            event.preventDefault();
+            this.reportValidity();
+        }
+    });
